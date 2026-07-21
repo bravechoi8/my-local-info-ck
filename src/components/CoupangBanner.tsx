a9b6778a842c5b4379ea4b9c5687ac13bdb2b008
my-local-info-user <user@example.com>
2026-07-21 13:26:44 +0900

    style: 쿠팡 배너 높이 150px 최적 설정값 최종 반영

--- src/components/CoupangBanner.tsx
+++ src/components/CoupangBanner.tsx
@@ -7,9 +7,9 @@ export default function CoupangBanner() {
     <div className="my-8 w-full overflow-hidden rounded-xl border border-slate-100 dark:border-slate-800 bg-white dark:bg-slate-900 p-4 shadow-sm">
       {/* 쿠팡 파트너스 공식 위젯 (수정/변형 없이 그대로 제공) */}
       <iframe
-        src={`https://ads-partners.coupang.com/widgets.html?id=987547&template=carousel&trackingCode=${partnerId}&subId=&width=100%&height=140&tsource=`}
+        src={`https://ads-partners.coupang.com/widgets.html?id=987547&template=carousel&trackingCode=${partnerId}&subId=&width=100%&height=150&tsource=`}
         width="100%"
-        height="140"
+        height="150"
         frameBorder="0"
         scrolling="no"
         referrerPolicy="unsafe-url"
